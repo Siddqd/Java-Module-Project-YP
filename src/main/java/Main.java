a4a14140c4bcaa77325d2872fea95f74f4b39694
siddqd <seedim@yandex.ru>
2025-09-21 10:54:18 +0400

    Добавлены проверки скорости на не инт значение и имени на пустое значение.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,47 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final int maxSpeed = 250;
-        final int minSpeed = 0;
+        final int minSpeed = 1;
         Scanner scan = new Scanner(System.in);
         Leader lead = new Leader();
-        int speed;
+        int speed = -1;
         String name;
-        {
-            System.out.print("\nВведите название " + 1 + " автомобиля: ");
+        for (int i = 1; i < 4; i++) {
+            System.out.print("\nВведите название " + i + " автомобиля: ");
             name = scan.next();
+//            System.out.println(name+" "+name.isEmpty());
+            while(name.isEmpty()) {
+                System.out.print("Название автомобиля не может быть пустым.\nВведите название " + i + " автомобиля: ");
+                name = scan.next();
+            }
+
             do {
-                System.out.print("Введите скорость " + 1 + " автомобиля: ");
+                System.out.print("Введите скорость " + i + " автомобиля: ");
+                while(!scan.hasNextInt()) {
+                    scan.next();
+                    System.out.print("Скорость должна быть числом.\nВведите скорость " + i + " автомобиля: ");
+                }
                 speed = scan.nextInt();
-                if (speed <= minSpeed || speed > maxSpeed)
-                    System.out.println("Скорость должна быть в пределах от 1 до 250 включительно");
-            } while (speed <= minSpeed || speed > maxSpeed);
-            Car newCar = new Car(name, speed);
-            lead.checkChamp(newCar);
+                if (speed < minSpeed || speed > maxSpeed)
+                    System.out.println("Скорость должна быть числом в пределах от "+(minSpeed)+" до "+maxSpeed+" включительно.");
+            } while (speed < minSpeed || speed > maxSpeed);
+            lead.checkChamp(new Car(name, speed));
         }
-        {
-            System.out.print("\nВведите название " + 2 + " автомобиля: ");
-            name = scan.next();
-            do {
-                System.out.print("Введите скорость " + 2 + " автомобиля: ");
-                speed = scan.nextInt();
-                if (speed <= 0 || speed > 250)
-                    System.out.println("Скорость должна быть в пределах от 1 до 250 включительно");
-            } while (speed <= 0 || speed > 250);
-            Car newCar = new Car(name, speed);
-            lead.checkChamp(newCar);
-        }
-        {
-            System.out.print("\nВведите название " + 3 + " автомобиля: ");
-            name = scan.next();
-            do {
-                System.out.print("Введите скорость " + 3 + " автомобиля: ");
-                speed = scan.nextInt();
-                if (speed <= 0 || speed > 250)
-                    System.out.println("Скорость должна быть в пределах от 1 до 250 включительно");
-            } while (speed <= 0 || speed > 250);
-            Car newCar = new Car(name, speed);
-            lead.checkChamp(newCar);
-        }
+
         System.out.println("\nСамая быстрая машина: "+lead.champName);
     }
 }
